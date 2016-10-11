ca38d93b89b4590ed9c8f11b921694f7a7a0f24e
mikamedioni <michael.medioni@gmail.com>
2016-10-11 17:20:50 +0200

    Implemented Algorithm

--- business/ElemWay.py
+++ business/ElemWay.py
@@ -1,5 +1,7 @@
+import business
+
 class ElemWay:
-    def __init__(self, type, duration, distance, price, steps):
+    def __init__(self, type='w', duration=0, distance=0, price=0, steps=[]):
         #TO DO : faire bien gaffe à comment les données sont récupérées
         self.type = type
         self.duration = duration
@@ -7,6 +9,14 @@ class ElemWay:
         self.price = price
         self.steps = steps
 
-    def __iter__(self):
-        
+a=ElemWay()
+a.price=10
+a.distance=20
+a.steps=["droite","gauche"]
+a.type='w'
 
+b=ElemWay()
+b.price=80
+b.distance=25
+b.steps=["gauche","gauche"]
+b.type='d'
--- business/Way.py
+++ business/Way.py
@@ -6,32 +6,29 @@ class Way:
         self.duration=0
         self.distance=0
         self.price=0
-        self.steps={}
         self.type=[]
 
 
     def __add__(self,other):
         waySum=Way()
         waySum.price = self.price + other.price
-        waySum.steps = self.steps + other.steps
         waySum.distance= self.distance + other.distance
         waySum.type = self.type
         waySum.elemWaysTable=self.elemWaysTable
 
         if isinstance(other,Way):
-            waySum.elemTable+=other.elemWaysTable
-            for type in other.type:
-                if type not in waySum.type:
-                    waySum.type+=type
+            waySum.elemWaysTable+=other.elemWaysTable
+            for i in range(len(other.type)):
+                if other.type[i] not in waySum.type:
+                    waySum.type+=other.type[i]
 
         else:
-            waySum.elemTable+=other.elemWaysTable
+            waySum.elemWaysTable.append(other)
 
             if other.type not in waySum.type:
                 waySum.type+=other.type
 
-        return waySum
-
+        return(waySum)
 
 
 
