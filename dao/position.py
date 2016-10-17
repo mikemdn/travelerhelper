8251255dc8e7cbcecb8d18872b822495c2ef85d7
Elodie Martin <elodie.martin@student.ecp.fr>
2016-10-17 11:51:54 +0200

    Refactor position class

--- dao/position.py
+++ dao/position.py
@@ -4,32 +4,27 @@ import constants
 
 class Position:
 
-    def __init__(self):
-        self.latitude = 0
-        self.longitude = 0
-        self.address = ""
+    def __init__(self, address):
+        self.address = address
+        self.latitude = Position.get_coordinates_from_address(self)[0]
+        self.longitude = Position.get_coordinates_from_address(self)[1]
 
     def __init__(self, latitude, longitude):
         self.latitude = latitude
         self.longitude = longitude
+        self.address = Position.get_address_from_coordinates(self)
 
-    def get_address_from_coordinates(self, latitude, longitude):
-        self.latitude = latitude
-        self.longitude = longitude
+    def get_address_from_coordinates(self):
         # Get address from Google Maps Geocoding API
         url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + str(self.latitude) + "," + str(self.longitude) + "&key=" + constants.google_maps_api_key
         result = requests.get(url).json()
-        self.address = result["results"][0]["formatted_address"]
+        return result["results"][0]["formatted_address"]
 
-    def get_coordinates_from_address(self, address):
-        self.address = address
+    def get_coordinates_from_address(self):
         # Get coordinates from Google Maps Geocoding API
         url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + self.address + "&key=" + constants.google_maps_api_key
         result = requests.get(url).json()
-        self.latitude = result["results"][0]["geometry"]["location"]["lat"]
-        self.longitude = result["results"][0]["geometry"]["location"]["lng"]
-        print(self.latitude)
-        print(self.longitude)
+        return result["results"][0]["geometry"]["location"]["lat"], result["results"][0]["geometry"]["location"]["lng"]
 
     def get_coordinates(self):
         return [self.latitude, self.longitude]
