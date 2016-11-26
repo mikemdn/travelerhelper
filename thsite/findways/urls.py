06fa306c0b31b36a57fecebed428d1f6e9de0ae9
mikamedioni <michael.medioni@gmail.com>
2016-11-26 12:32:41 +0100

    Fixed the django website

--- thsite/findways/urls.py
+++ thsite/findways/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
 
+app_name = 'findways'
+
 urlpatterns = [
     url(r'^', views.index, name = 'index')
 ]
--- thsite/findways/views.py
+++ thsite/findways/views.py
@@ -4,18 +4,21 @@ from django.views import generic
 
 # Create your views here.
 
-def index(request):
-    text = """<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Index</title>
-</head>
-<body>
-<h1>Bienvenue sur traveler helper</h1>
-<button>Se connecter</button>
-<button>S'inscrire</button>
+# def index(request):
+#     text = """<!DOCTYPE html>
+# <html lang="en">
+# <head>
+#     <meta charset="UTF-8">
+#     <title>Index</title>
+# </head>
+# <body>
+# <h1>Bienvenue sur traveler helper</h1>
+# <button>Se connecter</button>
+# <button>S'inscrire</button>
+#
+# </body>
+# </html>"""
+#     return HttpResponse(text)
 
-</body>
-</html>"""
-    return HttpResponse(text)
+def index(request):
+    return render(request,'findways/index.html')
